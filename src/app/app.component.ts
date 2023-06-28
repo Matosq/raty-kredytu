f3913278bcf78d8a976c806b792143135e8c222e
Matosq <mateuszgonet15@gmail.com>
2023-06-28 23:03:49 +0200

    Added inject DOCUMENT in app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,12 +10,14 @@ export class AppComponent implements OnInit {
   protected title = 'raty kredytu';
   private backgroundImageCssClassName = 'background';
 
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
   public ngOnInit(): void {
     this.lazyLoadBackgroundImage();
   }
 
   private lazyLoadBackgroundImage(): void {
-    const bodyElement = document.body;
+    const bodyElement = this.document.body;
     if (bodyElement) {
       bodyElement.classList.add(this.backgroundImageCssClassName);
     }
